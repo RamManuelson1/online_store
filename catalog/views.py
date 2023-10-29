ba40222cb742ec8c13034c176b7a7363ceb013c4
Roman Sharoyan <sharoyan24@yandex.ru>
2023-10-29 14:06:22 +0300

    2

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-def contacts(request):
-    return render(request, 'catalog/contacts.html')
-
 def home(request):
     return render(request, 'catalog/home.html')
+
+def contacts(request):
+    return render(request, 'catalog/contacts.html')
