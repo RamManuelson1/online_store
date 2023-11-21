9ba93fb457bff91e1e2f8598472b0df40208fddc
Roman Sharoyan <sharoyan24@yandex.ru>
2023-11-21 23:58:19 +0300

    3

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
 from catalog.views import contacts, home
+from . import views
+
 
 urlpatterns = [
     path('', home),
     path('contacts/', contacts),
+    path('<int:product_id>', views.product_detail, name='detail'),
+    path('', views.product_list, name='list'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
 def home(request):
     return render(request, 'catalog/home.html')
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return render(request, 'catalog/product.html', {'product': product})
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'catalog/list.html', {'products': products})
+
